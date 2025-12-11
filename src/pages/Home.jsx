14bdfc6d8b241308d052bd091ce0f57ec10a028c
FukudaMiyasato <fukudamiyasato@gmail.com>
2025-12-11 14:24:53 -0500

    mantenimiento

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -43,7 +43,7 @@ useEffect(() => {
             <span class="text-black font-semibold">Saldo disponible</span>
 
             <div class="bg-white px-6 py-2 rounded-2xl text-[#9B7AE0] text-xl font-bold">
-              s/95
+              s/94
             </div>
           </div>
 
@@ -52,7 +52,7 @@ useEffect(() => {
             <h2 class="text-lg font-bold text-black">Movimientos</h2>
             <hr class="border-black mt-1" />
             <Movimiento label="Kronologic Amazon" monto={95} tipo="ingreso" />
-            <Movimiento label="Prueba" monto={0} tipo="gasto" />
+            <Movimiento label="Mantenimiento" monto={1} tipo="gasto" />
 </div>
 
 
